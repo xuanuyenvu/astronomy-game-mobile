bddd49e2ece8c61f551d344218990f7244ef0f2e
Uyen Uyen <xuanuyenvu22@gmail.com>
2024-07-18 08:32:18 +0700

    fix IGamePlay

--- Assets/Scripts/Model/IGamePlay.cs
+++ Assets/Scripts/Model/IGamePlay.cs
@@ -1,4 +1,9 @@
 using UnityEngine;
-public abstract class IGamePlay : MonoBehaviour {
+public abstract class IGamePlay : MonoBehaviour
+{
+     [HideInInspector] public CameraShake cameraShake;
+     [HideInInspector] public CardController cardController;
+     [HideInInspector] public HealthManager healthManager;
+     [HideInInspector] public ScoreManager scoreManager;
      public abstract void Play();
 }
