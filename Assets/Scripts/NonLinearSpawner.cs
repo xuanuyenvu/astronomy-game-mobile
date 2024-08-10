2d03505c7f09f9ebe9d7ea7b3e797d680582b5ce
xuanuyenvu <xuanuyenvu22@gmail.com>
2024-08-10 14:55:43 +0700

    fix nonlinear

--- Assets/Scripts/NonLinearSpawner.cs
+++ Assets/Scripts/NonLinearSpawner.cs
@@ -39,6 +39,7 @@ public class NonLinearSpawner : IGamePlay
 
     RocketController destinationRocket = null;
     private Vector3 spawnRocketPosition = Vector3.zero;
+    private bool isHealthReduced = true;
 
     void Awake()
     {
@@ -63,6 +64,7 @@ public class NonLinearSpawner : IGamePlay
         {
             rocket.TurnOnCollider = false;
             destinationRocket.TurnOnCollider = false;
+            isHealthReduced = true;
         }
 
         if (cardController.GetNumOfCards() == 0)
@@ -248,9 +250,72 @@ public class NonLinearSpawner : IGamePlay
         rocket.gameObject.SetActive(true);
     }
 
+    private void SwapPlanetsByMetrics()
+    {
+        float distance1 = planet1.Distance(rocket);
+        float distance2 = planet2.Distance(rocket);
+
+        // Nêu hai planet giống nhau
+        // hoặc nếu rocket trùng trục x với 1 trong 2 planet
+        if (planet1.name == planet2.name 
+            || Mathf.Abs(rocket.transform.position.x - planet1.transform.position.x) < 1
+            || Mathf.Abs(rocket.transform.position.x - planet2.transform.position.x) < 1)
+        {
+            Debug.Log(" do a nha ");
+            if (distance1 > distance2)
+            {
+                MoveRocketCloserToPlanet(planet1);
+                rocket.RotateRocket(planet1.gameObject.transform.position);
+            }
+            else if (distance1 < distance2)
+            {
+                MoveRocketCloserToPlanet(planet2);
+                rocket.RotateRocket(planet1.gameObject.transform.position);
+            }
+        }
+
+        if (planet1.name != planet2.name)
+        {
+            float mass1 = (float)planet1.Mass;
+            float mass2 = (float)planet2.Mass;
+
+            // Kiểm tra nếu cả khoảng cách và khối lượng đều đồng điệu 
+            // tức là cùng cao hơn cái còn lại
+            bool isOrderCorrect = (distance1 > distance2 && mass1 > mass2)
+                                    || (distance1 < distance2 && mass1 < mass2);
+            Debug.Log("isOrderCorrect " + isOrderCorrect);
+            if (!isOrderCorrect)
+            {
+                SwapPlanetPosition();
+                rocket.RotateRocket(planet1.gameObject.transform.position);
+            }
+        }
+    }
+
+    private void MoveRocketCloserToPlanet(AstronomicalObject _planet, float moveDistance = 3.0f)
+    {
+        // Tính vector di chuyển từ rocket tới hành tinh
+        Vector3 directionToPlanet = (_planet.transform.position - rocket.transform.position).normalized;
+
+        Vector3 newRocketPosition = rocket.transform.position + directionToPlanet * moveDistance;
+
+        // Cập nhật vị trí mới cho rocket
+        rocket.transform.position = newRocketPosition;
+        spawnRocketPosition = newRocketPosition;
+    }
+
+    private void SwapPlanetPosition()
+    {
+        var tmp = planet1.transform.position;
+
+        planet1.transform.position = planet2.transform.position;
+        planet2.transform.position = tmp;
+        target.transform.position = tmp;
+    }
+
     public void SetDestinationOfRocket()
     {
-        Vector3 desPostion = CalculateResultantPosition(planet1, planet2, rocket).normalized * 2;
+        Vector3 desPostion = CalculateResultantPosition(planet1, planet2, rocket, 5);
 
         destinationRocket = Instantiate(rocketPrefab, desPostion, Quaternion.identity);
         destinationRocket.name = destinationRocket.name.Replace("meteorite(Clone)", "destination");
@@ -303,6 +368,7 @@ public class NonLinearSpawner : IGamePlay
     {
         RandomizePosition();
         SetRocket();
+        SwapPlanetsByMetrics();
         SetDestinationOfRocket();
         StartCoroutine(SetPositionBeforePlaying(0.5f));
 
@@ -375,7 +441,8 @@ public class NonLinearSpawner : IGamePlay
     }
 
     private Vector3 CalculateResultantPosition(AstronomicalObject _planet1,
-                                                AstronomicalObject _planet2, AstronomicalObject _rocket)
+                                                AstronomicalObject _planet2, AstronomicalObject _rocket,
+                                                float d)
     {
         // Tính vector lực hấp dẫn giữa hành tinh 1 và thiên thạch
         Vector3 force1 = _rocket.GetVectorAttractiveForce(_planet1);
@@ -385,24 +452,24 @@ public class NonLinearSpawner : IGamePlay
 
         // Tính vector hợp lực
         Vector3 resultantForce = force1 + force2;
-        resultantForce.z = -3;
+        // translatedResultantForce.z = -3;
 
-        return resultantForce + rocket.transform.position;
+        return (resultantForce.normalized * d) + rocket.transform.position;;
     }
 
     private void RocketFlyAnimation()
     {
         // Nếu kết quả đúng thì xoay rocket
-        if (planetAnswer.name == planet2.name)
-        {
-            // Lắc rocket và hiển thị hiệu ứng correct
-            destinationRocket.TurnOnCollider = true;
-            StartCoroutine(CoroutineCorrectAnwser());
-            scoreManager.FinalScore(healthManager.health);
-            return;
-        }
+        // if (planetAnswer.name == planet2.name)
+        // {
+        //     // Lắc rocket và hiển thị hiệu ứng correct
+        //     destinationRocket.TurnOnCollider = true;
+        //     StartCoroutine(CoroutineCorrectAnwser());
+        //     scoreManager.FinalScore(healthManager.health);
+        //     return;
+        // }
 
-        Vector3 resultant = CalculateResultantPosition(planet1, planetAnswer, rocket);
+        Vector3 resultant = CalculateResultantPosition(planet1, planetAnswer, rocket, 20);
         // resultant.Normalize();
         StartCoroutine(rocket.ShakeAndFlyTo(resultant));
     }
@@ -460,7 +527,11 @@ public class NonLinearSpawner : IGamePlay
         cameraShake.ShakeCamera();
 
         // Mất 1 mạng
-        healthManager.health--;
+        if (isHealthReduced == true)
+        {
+            isHealthReduced = false;
+            healthManager.health--;
+        }
         yield return null;
     }
 
