5826a2131856d7b256e665efbde7c8105c0f428d
xuanuyenvu <xuanuyenvu22@gmail.com>
2024-08-21 19:45:35 +0700

    level menu

---
+++ Assets/Scripts/LevelButtonHandler.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class LevelButtonHandler : MonoBehaviour
+{
+    private Button unlockedButton = null;
+    private Button lockedButton = null;
+
+    public int level;
+
+    private void Awake()
+    {
+        unlockedButton = transform.Find("unlockedLevel").GetComponent<Button>();
+        lockedButton = transform.Find("lockedLevel").GetComponent<Button>();
+
+        string levelText;
+        if (level < 10)
+        {
+            levelText = "0" + level.ToString();
+        }
+        else
+        {
+            levelText = level.ToString();
+        }
+
+        TextMeshProUGUI ubText = unlockedButton.GetComponentInChildren<TextMeshProUGUI>();
+        ubText.text = level.ToString();
+
+        TextMeshProUGUI lbText = lockedButton.GetComponentInChildren<TextMeshProUGUI>();
+        lbText.text = level.ToString();
+    }
+
+    public void LockThisButton()
+    {
+        unlockedButton.gameObject.SetActive(false);
+        lockedButton.gameObject.SetActive(true);
+        lockedButton.interactable = false;
+    }
+
+    public void UnlockThisButton()
+    {
+        unlockedButton.gameObject.SetActive(true);
+        lockedButton.gameObject.SetActive(false);
+    }
+}
+
