050e51932e326d862fe26303398699d4e5d52f63
xuanuyenvu <xuanuyenvu22@gmail.com>
2024-10-23 23:31:09 +0700

    fix UI chapter menu

--- Assets/Scripts/ChapterMenu.cs
+++ Assets/Scripts/ChapterMenu.cs
@@ -2,47 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class ChapterMenu : MonoBehaviour
 {
-    public ParticleSystem particleSystem1;
+    public int star = 10;
+    public Button chapter2;
     public ParticleSystem particleSystem2;
-
     void Start()
     {
-        particleSystem1.Stop();
-        particleSystem2.Stop();
-        particleSystem1.gameObject.SetActive(false);
-        particleSystem2.gameObject.SetActive(false);
+        if (star >= 12)
+        {
+
+            chapter2.interactable = true;
+            particleSystem2.Play();
+        }
+        else
+        {
+            chapter2.interactable = false;
+            particleSystem2.Stop();
+        }
     }
-
-
     public void LoadChapter1()
     {
-        particleSystem1.gameObject.SetActive(true);
-        particleSystem1.Play();
-        StartCoroutine(WaitAndLoadScene1());
-    }
-
-    private IEnumerator WaitAndLoadScene1()
-    {
-        yield return new WaitForSeconds(1.5f);
         Debug.Log("Chapter 1");
-        SceneManager.LoadScene("chapter1");  
+        // SceneManager.LoadScene("chapter1");
     }
-
 
     public void LoadChapter2()
     {
-        particleSystem2.gameObject.SetActive(true);
-        particleSystem2.Play();
-        StartCoroutine(WaitAndLoadScene2());
-    }
-
-    private IEnumerator WaitAndLoadScene2()
-    {
-        yield return new WaitForSeconds(1.5f);
         Debug.Log("Chapter 2");
-        SceneManager.LoadScene("chapter1");
+        // SceneManager.LoadScene("chapter2");
     }
 }
