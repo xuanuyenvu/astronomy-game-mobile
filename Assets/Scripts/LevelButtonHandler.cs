a067d14e0499889ae72a38d119fc3880e1959008
Uyen Vu <xuanuyenvu22@gmail.com>
2024-11-13 12:18:23 +0700

    level

--- Assets/Scripts/LevelButtonHandler.cs
+++ Assets/Scripts/LevelButtonHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,50 +7,142 @@ using UnityEngine.UI;
 
 public class LevelButtonHandler : MonoBehaviour
 {
-    public Button unlockedButton;
-    public Button lockedButton;
+    public Image largeCircle;
+    public Image mediumCircle;
+    public Image star;
+    public Image rock;
+    public TextMeshProUGUI levelText;
+    [SerializeField] public int state = 1;
+    [SerializeField] private int level = 1;
+    
+    public Button button;
 
-    public int level;
-
-    private void Awake()
+    private void Start()
     {
-        if (unlockedButton == null)
-        {
-            unlockedButton = transform.Find("unlockedLevel").GetComponent<Button>();
-        }
-        if (lockedButton == null)
-        {
-            lockedButton = transform.Find("lockedLevel").GetComponent<Button>();
-        }
+        SetLevel(level);
+    }
 
-        string levelText;
+    private void SetLevel(int level)
+    {
         if (level < 10)
         {
-            levelText = "0" + level.ToString();
+            levelText.text = "0" + level;
         }
         else
         {
-            levelText = level.ToString();
+            levelText.text = level.ToString();
         }
-
-        TextMeshProUGUI ubText = unlockedButton.GetComponentInChildren<TextMeshProUGUI>();
-        ubText.text = levelText;
-
-        TextMeshProUGUI lbText = lockedButton.GetComponentInChildren<TextMeshProUGUI>();
-        lbText.text = levelText;
+    }
+    
+    public int GetLevel()
+    {
+        return level;
     }
 
-    public void LockThisButton()
+    private void OnValidate()
     {
-        unlockedButton.gameObject.SetActive(false);
-        lockedButton.gameObject.SetActive(true);
-        lockedButton.interactable = false;
+        UpdateState(); // Cập nhật giao diện khi giá trị state thay đổi từ Inspector
     }
 
-    public void UnlockThisButton()
+    private void UpdateState()
     {
-        unlockedButton.gameObject.SetActive(true);
-        lockedButton.gameObject.SetActive(false);
+        switch (state)
+        {
+            case 1:
+                SetLocked();
+                break;
+            case 2:
+                SetUnlockedUnplayed();
+                break;
+            case 3:
+                SetPlayedRocket();
+                break;
+            case 4:
+                SetPlayedStar();
+                break;
+        }
+    }
+
+    private void SetLocked()
+    {
+        button.interactable = true;
+        rock.gameObject.SetActive(false);
+        star.gameObject.SetActive(false);
+
+        largeCircle.color = Color.white;
+        largeCircle.color = new Color(largeCircle.color.r, largeCircle.color.g, largeCircle.color.b, 80f / 255f);
+        mediumCircle.color = Color.white;
+        mediumCircle.color = new Color(mediumCircle.color.r, mediumCircle.color.g, mediumCircle.color.b, 30f / 255f);
+        levelText.alpha = 80f / 255f;
+
+        RectTransform rectTransform = levelText.GetComponent<RectTransform>();
+        if (rectTransform != null)
+        {
+            // rectTransform.offsetMin = Vector2.zero;
+            // rectTransform.offsetMax = Vector2.zero;
+            rectTransform.anchoredPosition = new Vector3(0, 0, 0);
+        }
+    }
+
+    private void SetUnlockedUnplayed()
+    {
+        button.interactable = false;
+        rock.gameObject.SetActive(false);
+        star.gameObject.SetActive(false);
+
+        largeCircle.color = Color.white;
+        largeCircle.color = new Color(largeCircle.color.r, largeCircle.color.g, largeCircle.color.b, 255f / 255f);
+        mediumCircle.color = Color.white;
+        mediumCircle.color = new Color(mediumCircle.color.r, mediumCircle.color.g, mediumCircle.color.b, 50f / 255f);
+
+        levelText.alpha = 225f / 255f;
+        RectTransform rectTransform = levelText.GetComponent<RectTransform>();
+        if (rectTransform != null)
+        {
+            // rectTransform.offsetMin = Vector2.zero;
+            // rectTransform.offsetMax = Vector2.zero;
+            rectTransform.anchoredPosition = new Vector3(0, 0, 0);
+        }
+    }
+
+    private void SetPlayedRocket()
+    {
+        button.interactable = false;
+        rock.gameObject.SetActive(true);
+        star.gameObject.SetActive(false);
+        
+        SetColorWhenPlayed();
+        
+        RectTransform rectTransform = levelText.GetComponent<RectTransform>();
+        if (rectTransform != null)
+        {
+            // rectTransform.offsetMin = new Vector2(-96, -30);
+            // rectTransform.offsetMax = new Vector2(96, 30);
+            rectTransform.anchoredPosition = new Vector3(170, -93, 0);
+        }
+    }
+
+    private void SetPlayedStar()
+    {
+        // button.interactable = false;
+        rock.gameObject.SetActive(false);
+        star.gameObject.SetActive(true);
+        
+        SetColorWhenPlayed();
+        
+        RectTransform rectTransform = levelText.GetComponent<RectTransform>();
+        if (rectTransform != null)
+        {
+            // rectTransform.offsetMin = new Vector2(-96, -30);
+            // rectTransform.offsetMax = new Vector2(96, 30);
+            rectTransform.anchoredPosition = new Vector3(170, -93, 0);
+        }
+    }
+
+    private void SetColorWhenPlayed()
+    {
+        largeCircle.color = new Color(177f / 255f, 192f / 255f, 255f / 255f, 200f / 255f);
+        mediumCircle.color = new Color(50f / 255f, 83f / 255f, 157f / 255f, 255f / 255f);
+        levelText.alpha = 225f / 255f;
     }
 }
-
